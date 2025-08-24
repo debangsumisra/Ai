dba9c44655757daeb2c88cf06829304dbacbfc17
debangsumisra <debangsumisra2005@gmail.com>
2025-08-25 00:06:55 +0530

    Updated BFS code

--- graph1.py
+++ graph1.py
@@ -1,19 +1,15 @@
 def BFS(graph, start):
-    # Initialization
     color = {u: "WHITE" for u in graph}
     distance = {u: float("inf") for u in graph}
     parent = {u: None for u in graph}
-
-    # Start node setup
+    
     color[start] = "GRAY"
     distance[start] = 0
     parent[start] = None
-
-    # Queue initialization
-    Q =[]
+    
+    Q = []
     Q.append(start)
-
-    # BFS loop
+    
     while Q:
         u = Q.pop(0)
         for v in graph[u]:
@@ -25,7 +21,9 @@ def BFS(graph, start):
         color[u] = "BLACK"
 
     return distance, parent
-dict1={"c":["F"],"E":["F"],"B":["D"],"A":["B","C"],"F":[],"D":[]}
-BFS(dict1,"A")
 
-     
+dict1 = {"C":["F"], "E":["F"], "B":["D"], "A":["B","C"], "F":[], "D":[]}
+
+dist, parent = BFS(dict1,"A")
+print("Distance:", dist)
+print("Parent:", parent)
